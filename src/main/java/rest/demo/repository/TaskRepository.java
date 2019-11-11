170d9163621f9f7ec7cc45246b5936bf16f4f81a
Marcin <marcinkulik@icloud.com>
2019-11-11 00:22:12 +0000

    removing unnecessary repository

--- src/main/java/rest/demo/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package rest.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rest.demo.model.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
